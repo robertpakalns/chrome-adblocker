744364541018d6a5c61fca9f392d337ea6f4b241
rob <roby.pakalns@gmail.com>
2025-05-19 21:16:04 +0300

    refactoring; toggle options

--- adblocker.js
+++ adblocker.js
@@ -1,42 +1,33 @@
-const adDomains = [
-    "adinplay.com",
-    "amazon-adsystem.com",
-    "doubleclick.net",
-    "imasdk.googleapis.com",
-    "pagead2.googlesyndication.com",
-    "ads-twitter.com",
-    "image6.pubmatic.com",
-    "googleadservices.com",
-    "adrecover.com",
-    "bet365partners.com",
+const loadDomainList = async category => {
+    const url = chrome.runtime.getURL(`assets/jsons/${category}.json`)
+    const res = await fetch(url)
+    return res.json()
+}
 
-    "adbox.lv"
-]
+const updateRules = async () => {
+    const { enabledCategories = [] } = await chrome.storage.local.get("enabledCategories")
 
-const trackerDomains = [
-    "google-analytics.com",
-    "googletagmanager.com"
-]
+    const lists = await Promise.all(enabledCategories.map(loadDomainList))
+    const domainsToBlock = lists.flat()
 
-const maliciousDomains = [
-    "steamecommunjty.com",
-    "sneamcommnumnlty.com",
-    "steamconmmunity.cfd",
-    "steamecommunnity.cfd",
-    "casumonster.top",
-    "e.vg"
-]
+    const rules = domainsToBlock.map((domain, index) => ({
+        id: index + 1,
+        action: { type: "block" },
+        condition: {
+            urlFilter: domain,
+            resourceTypes: ["main_frame", "sub_frame", "script", "image"]
+        }
+    }))
 
-const rules = [...trackerDomains, ...adDomains, ...maliciousDomains].map((domain, index) => ({
-    id: index + 1,
-    action: { type: "block" },
-    condition: {
-        urlFilter: domain,
-        resourceTypes: ["main_frame", "sub_frame", "script", "image"]
+    await chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: Array.from({ length: 1000 }, (_, i) => i + 1),
+        addRules: rules
+    })
+}
+
+chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
+    if (message.type === "updateRules") {
+        updateRules().then(() => sendResponse({ success: true }))
+        return true
     }
-}))
-
-chrome.declarativeNetRequest.updateDynamicRules({
-    addRules: rules,
-    removeRuleIds: rules.map(rule => rule.id)
 })
--- ui/script.js
+++ ui/script.js
@@ -1,1 +1,20 @@
-document.querySelector("#button").addEventListener("click", () => alert("Tricko Guard"))
+const checkboxes = {
+    ads: document.getElementById("ads"),
+    trackers: document.getElementById("trackers"),
+    malicious: document.getElementById("malicious")
+}
+
+const loadSettings = async () => {
+    const { enabledCategories = [] } = await chrome.storage.local.get("enabledCategories")
+    for (const key in checkboxes) checkboxes[key].checked = enabledCategories.includes(key)
+}
+
+const saveSettings = async () => {
+    const enabledCategories = Object.keys(checkboxes).filter(key => checkboxes[key].checked)
+    await chrome.storage.local.set({ enabledCategories })
+    chrome.runtime.sendMessage({ type: "updateRules" })
+}
+
+for (const key in checkboxes) checkboxes[key].addEventListener("change", saveSettings)
+
+loadSettings()
