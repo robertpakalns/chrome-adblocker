5e7f6091feef071ca06a6336cdad37b97839c088
rob <roby.pakalns@gmail.com>
2025-05-19 21:38:16 +0300

    hint on checkbox change

--- ui/script.js
+++ ui/script.js
@@ -3,17 +3,36 @@ const checkboxes = {
     trackers: document.getElementById("trackers"),
     malicious: document.getElementById("malicious")
 }
+const hintElement = document.getElementById("hint")
+let hintTimeout = null
 
 const loadSettings = async () => {
+    hintElement.innerText = ""
+
     const { enabledCategories = [] } = await chrome.storage.local.get("enabledCategories")
     for (const key in checkboxes) checkboxes[key].checked = enabledCategories.includes(key)
+}
+
+const showHint = text => {
+    hintElement.innerText = text
+    hintElement.classList.add("visible")
+
+    if (hintTimeout !== null) clearTimeout(hintTimeout)
+    hintTimeout = setTimeout(() => {
+        hintElement.classList.remove("visible");
+        setTimeout(() => hintElement.innerText = "", 500)
+        hintTimeout = null
+    }, 3000)
 }
 
 const saveSettings = async () => {
     const enabledCategories = Object.keys(checkboxes).filter(key => checkboxes[key].checked)
     await chrome.storage.local.set({ enabledCategories })
     chrome.runtime.sendMessage({ type: "updateRules" })
+
+    showHint("Refresh the page to apply the changes")
 }
+
 
 for (const key in checkboxes) checkboxes[key].addEventListener("change", saveSettings)
 
