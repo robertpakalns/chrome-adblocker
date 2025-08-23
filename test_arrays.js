70eba9e99d733efd9378849ef8fd3cd36adfe49c
rob <roby.pakalns@gmail.com>
2025-08-23 21:08:04 +0300

    update array tester script

--- test_arrays.js
+++ test_arrays.js
@@ -1,8 +1,10 @@
+import { writeFileSync } from "fs";
+import { join, resolve } from "path";
 import adsRaw from "./assets/jsons/ads.json" with { type: "json" };
 import maliciousRaw from "./assets/jsons/malicious.json" with { type: "json" };
 import trackersRaw from "./assets/jsons/trackers.json" with { type: "json" };
 
-const processList = (raw, name) => {
+const processList = (raw, name, path) => {
   let processed = false;
   const lowerList = raw.map((el) => el.toLowerCase());
   const list = [...new Set(lowerList)];
@@ -28,16 +30,19 @@ const processList = (raw, name) => {
   const processedList = [...list.filter((el) => !toRemove.has(el))].sort();
   if (JSON.stringify(lowerList) !== JSON.stringify(processedList)) {
     processed = true;
-    console.log(`❌ ${name}: list was not sorted, fixing...`);
+    console.log(`❌ ${name}: list was not sorted or had conflicts, fixing...`);
   }
 
   if (!processed) {
     console.log(`✅ ${name}: no conflicts found.`);
   } else {
-    console.log("✅ Processed list:\n", JSON.stringify(processedList));
+    console.log(`✅ ${name}: writing processed list to file...`);
+    writeFileSync(path, JSON.stringify(processedList, null, 2), "utf-8");
   }
 };
 
-processList(adsRaw, "Ads");
-processList(maliciousRaw, "Malicious");
-processList(trackersRaw, "Trackers");
+const jsonsDir = resolve("./assets/jsons");
+
+processList(adsRaw, "Ads", join(jsonsDir, "ads.json"));
+processList(maliciousRaw, "Malicious", join(jsonsDir, "malicious.json"));
+processList(trackersRaw, "Trackers", join(jsonsDir, "trackers.json"));
